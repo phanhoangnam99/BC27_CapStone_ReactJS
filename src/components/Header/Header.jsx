9fa3e66a19ad93c59240a7fcb764fea3ff49cf76
NamPhan <nam230687@gmail.com>
2022-09-21 00:38:08 +0700

    'final'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
-import { Button } from "antd";
+import { Avatar, Button, Popover } from "antd";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import './header.css'
-const Header = () => {
+import { logout } from "../../modules/Authentication/slices/authSlice";
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 
+const Header = () => {
+  const { user } = useSelector((state) => state.auth)
+
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   return (
     <Navbar>
@@ -14,11 +26,36 @@ const Header = () => {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQDYau3Hs4-xw1i8jVSUY4BlF4FLmg8lQqg&usqp=CAU"
             alt=""
           />
-          <span onClick={()=>navigate('/')}>Homepage</span>
+          <span onClick={() => navigate('/')}>Homepage</span>
           <span>Movies</span>
           <span onClick={() => navigate('/login')}>Login</span>
-          <span onClick={()=>navigate('/register')} > Register</span>
-          <span onClick={()=>navigate('/admin')} > Chuyển qua trang Admin</span>
+          <span onClick={() => navigate('/register')} > Register</span>
+          <span onClick={() => navigate('/admin')} > Chuyển qua trang Admin</span>
+          {user ?
+            <div className='d-flex' style={{
+              position: "absolute",
+              right: "60px",
+            }}>
+              <Avatar style={{ backgroundColor: '#87d068', margin: 'auto 20px' }} icon={<UserOutlined />} />
+
+              <Popover content={
+                <>
+                  <a a id='logout' type='primary' onClick={async () => {
+                    await dispatch(logout()).unwrap();
+
+                  }}>Đăng xuất</a>
+
+
+                </>
+
+
+
+              }>
+                <h3 style={{ margin: 'auto 0 ', color: '#fff' }}>{user?.taiKhoan}</h3>
+              </Popover>
+            </div>
+            :
+            null}
         </div>
         <div className=""></div>
       </div>
