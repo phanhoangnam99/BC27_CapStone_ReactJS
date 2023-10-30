f7c64168b71e805654859f36396f72f7f89acc75
nam <johndoe@example.com>
2023-10-30 09:27:14 +0700

    update

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -5,9 +5,8 @@ import store from "../store";
 const axiosClient = axios.create({
   baseURL: "https://movienew.cybersoft.edu.vn/api/",
   headers: {
-    TokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MiIsIkhldEhhblN0cmluZyI6IjMwLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NjAzMjAwMDAwMCIsIm5iZiI6MTY2NzA2MjgwMCwiZXhwIjoxNjk2MTc5NjAwfQ.i6JqYnGkwyHl6dkDHnjFWbPfBEl2l4SXAp4r7h9Ecpw",
-  },
+    TokenCybersoft:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJDT0RFIEPDmU5HIE1FTlRPUiBDQSBDSEnhu4BVICIsIkhldEhhblN0cmluZyI6IjAxLzAzLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0MDc4NzIwMDAwMCIsIm5iZiI6MTcwMjU3MzIwMCwiZXhwIjoxNzQwOTM0ODAwfQ.9mLBousObMWvuGV-RLRGZBBDVQrX8jtgI7y3CovcXuM"
+      },
 });
 
 // interceptors
